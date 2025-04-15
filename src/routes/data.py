ca8609e266a6e299892189982d866f984b01a351
Mohamed Ibrahim <mohamed3ibrahim20@gmail.com>
2025-04-15 16:58:12 +0200

    data processing

--- src/routes/data.py
+++ src/routes/data.py
@@ -2,10 +2,11 @@ from fastapi import FastAPI, APIRouter, Depends, UploadFile, status
 from fastapi.responses import JSONResponse
 import os
 from helpers.config import get_settings, Settings
-from controllers import DataController, ProjectController
+from controllers import DataController, ProjectController, ProcessController
 import aiofiles
 from models import ResponseSignal
 import logging
+from .schemes.data import ProcessRequest 
 
 logger = logging.getLogger('uvicorn.error')
 
@@ -18,7 +19,6 @@ data_router = APIRouter(
 async def upload_data(project_id: str, file: UploadFile,
                       app_settings: Settings = Depends(get_settings)):
         
-    
     # validate the file properties
     data_controller = DataController()
 
@@ -59,3 +59,32 @@ async def upload_data(project_id: str, file: UploadFile,
                 "file_id": file_id
             }
         )
+
+@data_router.post("/process/{project_id}")
+async def process_endpoint(project_id: str, process_request: ProcessRequest):
+    
+    file_id = process_request.file_id
+    chunk_size = process_request.chunk_size
+    overlab_size = process_request.overlab_size
+
+
+    process_controller = ProcessController(project_id=project_id)
+
+    file_content = process_controller.get_file_content(file_id=file_id)
+
+    file_chunks = process_controller.process_file_content(
+        file_content= file_content,
+        file_id= file_id,
+        chunk_size= chunk_size,
+        overlap_size=overlab_size
+    )
+
+    if file_chunks is None or len(file_chunks) ==0:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": ResponseSignal.PROCESSING_FAILED.value
+            }
+        )
+
+    return file_chunks
